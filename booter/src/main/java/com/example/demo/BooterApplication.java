4d336b67f69a4815af0d882e5359efc05a82e185
tommy <13755093906@139.com>
2018-08-09 10:17:45 +0800

    atest

---
+++ booter/src/main/java/com/example/demo/BooterApplication.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@SpringBootApplication
+public class BooterApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooterApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BooterApplication.class);
+	}
+	@RequestMapping("/")
+	public String home() {
+		return "你好，亮！";
+	}
+}
